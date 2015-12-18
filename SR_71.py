8d97d99aa8546484d08240a6737389860dbc2e81
Timothy S. Winters <twinters007@gmail.com>
2015-12-18 10:11:48 -0500

    Added SR_71 class

---
+++ SR_71.py
@@ -0,0 +1,42 @@
+import sys, enum
+class Color(enum.Enum):
+    RED ='\033[0;31m'
+    GREEN ='\033[0;32m'
+    YELLOW ='\033[1;33m'
+    CYAN ='\033[0;36m'
+    BCYAN ='\033[1;36m'
+    PURPLE ='\033[0;35m'
+    GRAY ='\033[0;37m'
+    MAGENTA ='\033[0;37m'
+    NC ='\033[0m'
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
--- users.py
+++ users.py
@@ -1,8 +1,14 @@
 import shlex, subprocess, re
-command_line = "wmic UserAccount get Name"
-args = shlex.split(command_line)
-p = subprocess.Popen(args, stdout=subprocess.PIPE)
-(out, err) = p.communicate()
-a = re.findall(r"[\w']+", out.decode())
-print(a)
 
+from SR_71 import query_yes_no, Color
+
+#command_line = "wmic UserAccount get Name"
+#args = shlex.split(command_line)
+#p = subprocess.Popen(args, stdout=subprocess.PIPE)
+#(out, err) = p.communicate()
+#a = re.findall(r"[\w']+", out.decode())
+for user in ["Tim",     "John", "Phil"]:
+    if query_yes_no("Is %s an authorized user?" % user):
+        print("Allowing user %s to stay" % user)
+    else:
+        print("Removing user "+ Color.RED.value + "%s" % user + Color.NC.value)
